5351e273f5187bc1ad4e01d8ff6a0d59e6ced2c3
Siddharth <officialsiddharthbisht@gmail.com>
2022-09-15 08:39:00 +0530

    FULL-PAGE-SCREENSHOT: successfully taken full page screenshot

--- ss.js
+++ ss.js
@@ -5,6 +5,11 @@ async function webScrap(){
     const page = await browser.newPage();
     await page.goto("https://learnwebcode.github.io/practice-requests/")
     await page.screenshot({path:"./screenshot/amazing.png"});
+
+    // for full page screeshot
+    const page1 = await browser.newPage();
+    await page1.goto("https://en.wikipedia.org/wiki/JavaScript");
+    await page1.screenshot({path:"./screenshot/fullpage.png",fullPage:true});
     await browser.close();
 }
 
