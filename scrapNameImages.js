3d467503345bc4244ad554e90b6f76724997984d
Siddharth <officialsiddharthbisht@gmail.com>
2022-09-15 10:33:16 +0530

    ADDED-COMMENTS: added explainer comments and indentation

--- scrapNameImages.js
+++ scrapNameImages.js
@@ -8,12 +8,15 @@ async function start(){
 
     await page.goto("https://learnwebcode.github.io/practice-requests/")
 
+    // * Web Scraping Names
     const names = await page.evaluate(()=>{
         let nodelist = document.querySelectorAll(".info strong"); //will retrun node list of elements
         return Array.from(nodelist).map(x =>x.textContent)
     })
 
     await fs.writeFile("./names/names.txt",names.join("\n"));
+    
+    // * Web Scraping photos
     // instead pf arr.from and all we can select multiple elements by $$eval("css slector",function(array having css seletor elements))
     const photos = await page.$$eval("img", (imgs)=>{
         return imgs.map(x => x.src);
@@ -23,6 +26,7 @@ async function start(){
         const imagePage = await page.goto(photo);
         await fs.writeFile("./images/"+photo.split("/").pop(),await imagePage.buffer());
     }
+    // web scraping code after this won't work since goto is pointing to different pages for images
     await browser.close();
 }
 
